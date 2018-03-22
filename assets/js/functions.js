8bdd0221bc706c6212e4e6ed2fb4ca73741fe11c
Nathan Workman <nathancworkman@gmail.com>
2018-03-22 11:21:24 -0400

    Cleanup boostrap nav

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -31,38 +31,54 @@ $(document).ready(function() {
  $('.stop').on('click', function() {owl.trigger('stop.owl.autoplay')})
 })
 // Nav toggle at 767px for sub-menu
-$(document).ready(function(){
-  $("#chev1").click(function(){
-      $("#sub1").slideToggle();
-  });
-});
-$(document).ready(function(){
-  $("#chev2").click(function(){
-      $("#sub2").slideToggle();
-  });
-});
-$(document).ready(function(){
-  $("#chev3").click(function(){
-      $("#sub3").slideToggle();
-  });
-});
-$(document).ready(function(){
-  $("#chev4").click(function(){
-      $("#sub4").slideToggle();
-  });
-});
-$(document).ready(function(){
-  $("#chev5").click(function(){
-      $("#sub5").slideToggle();
-  });
-});
-$(document).ready(function(){
-  $("#chev6").click(function(){
-      $("#sub6").slideToggle();
-  });
-});
-$(document).ready(function(){
-  $("#chev7").click(function(){
-      $("#sub7").slideToggle();
-  });
+// $(document).ready(function(){
+//   $("#chev1").click(function(){
+//       $("#sub1").slideToggle();
+//   });
+// });
+// $(document).ready(function(){
+//   $("#chev2").click(function(){
+//       $("#sub2").slideToggle();
+//   });
+// });
+// $(document).ready(function(){
+//   $("#chev3").click(function(){
+//       $("#sub3").slideToggle();
+//   });
+// });
+// $(document).ready(function(){
+//   $("#chev4").click(function(){
+//       $("#sub4").slideToggle();
+//   });
+// });
+// $(document).ready(function(){
+//   $("#chev5").click(function(){
+//       $("#sub5").slideToggle();
+//   });
+// });
+// $(document).ready(function(){
+//   $("#chev6").click(function(){
+//       $("#sub6").slideToggle();
+//   });
+// });
+// $(document).ready(function(){
+//   $("#chev7").click(function(){
+//       $("#sub7").slideToggle();
+//   });
+// });
+
+// $('.dropdown-toggle').click(function(e) {
+//   if ($(document).width() < 768) {
+//     e.preventDefault();
+//     var url = $(this).attr('href');
+//     if (url !== '#') {
+//       window.location.href = url;
+//     }
+//   }
+// });
+
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
--- includes/footer.php
+++ includes/footer.php
@@ -10,12 +10,12 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="/assets/js/functions.js"></script>
-    <script src="/assets/js/bootstrap.min.js"></script>
+<!--<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>-->
+    <!-- <script src="/assets/js/bootstrap.min.js"></script> -->
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="/assets/js/ie10-viewport-bug-workaround.js"></script>
     <!-- Owl carousel js -->
     <script src="/assets/js/owl.carousel.min.js"></script>
+    <script src="/assets/js/functions.js"></script>
 </body>
 </html>
