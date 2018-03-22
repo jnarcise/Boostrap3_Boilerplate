b1182c786e3cab722002a574e81efc09d69be43a
Nathan Workman <nathancworkman@gmail.com>
2018-03-22 11:36:48 -0400

    Remove duplicate scripts

--- includes/footer.php
+++ includes/footer.php
@@ -3,19 +3,15 @@
   <p class="text-center">&copy; <?php echo date("Y"); ?></p>
 </footer>
 <!-- /. -->
-<!-- Bootstrap core JavaScript
-    ================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
 <!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!-- Latest compiled JavaScript -->
+<!-- Bootstrap -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<!--<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>-->
-    <!-- <script src="/assets/js/bootstrap.min.js"></script> -->
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="/assets/js/ie10-viewport-bug-workaround.js"></script>
-    <!-- Owl carousel js -->
-    <script src="/assets/js/owl.carousel.min.js"></script>
-    <script src="/assets/js/functions.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="/assets/js/ie10-viewport-bug-workaround.js"></script>
+<!-- Owl carousel js -->
+<script src="/assets/js/owl.carousel.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="/assets/js/functions.js"></script>
 </body>
 </html>
